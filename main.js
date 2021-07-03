70e3b771a9b539b6ad9512c973cfa20e2bbfa270
Bouchou Djessim <djessim.bouchou@ensta-paris.fr>
2021-07-03 12:26:06 +0200

    advance

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: "Socks",
+            description:"Hmmm... C'est chaud là dedans"
+        }
+    },
+})
